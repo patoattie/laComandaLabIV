9db4a76e15b22a62b52af57f7c257e3bbfdbb82e
patoattie <patoattie@gmail.com>
2019-12-14 18:13:10 -0300

    configuracion inicial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBJpiE74icuJcqH7eMvtBxM5Yo77flP7HE",
+    authDomain: "lacomandalabiv.firebaseapp.com",
+    databaseURL: "https://lacomandalabiv.firebaseio.com",
+    projectId: "lacomandalabiv",
+    storageBucket: "lacomandalabiv.appspot.com",
+    messagingSenderId: "873790294335",
+    appId: "1:873790294335:web:a07d532c4c5b037dfbab8a"
+  }
 };
 
 /*
